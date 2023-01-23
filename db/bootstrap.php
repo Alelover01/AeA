a76a1578fcd37c67d2f58117df7b3f5c18a173ce
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-01-23 15:09:49 +0100

    Update bootstrap.php

--- db/bootstrap.php
+++ db/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once("functions.php");
+require_once("php/functions.php");
 require_once("database.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "see_go", 3306);
 ?>
