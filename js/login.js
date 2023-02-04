b038b7644f220b9fd136d144edc5de4a7c4b6268
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-04 12:45:16 +0100

    updated login.js

--- js/login.js
+++ js/login.js
@@ -1,7 +1,18 @@
-axios.get('php/login.php').then(response => {
-    if(response.data["logineseguito"]){
-        window.location.replace("./home.php");   
-    }else{
-        window.location.reload;  
-    }
+function myFunction() {
+    alert("Password o Username sbagliati!");
+};
+
+axios.get('php/api-login.php').then(response => {
+    
+    let btn_submit = document.getElementById("btn_submit");
+    btn_submit.addEventListener("click", () => {
+        console.log(response);
+        if(response.data["logineseguito"]){
+            window.location.replace("./home.php");   
+        }else{
+            alert("Password o Username sbagliati!");
+            //myFunction();
+            window.location.reload;  
+        }
+    });
 });
