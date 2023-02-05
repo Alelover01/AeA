e16931df408a511092f4e847573418389a6078ff
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-05 16:34:42 +0100

    Updated functions.php

--- php/functions.php
+++ php/functions.php
@@ -2,16 +2,15 @@
 
 //funzione che mi dice se un utente è loggato
 function isUserLoggedIn(){
-    return !empty($_SESSION['Username']);
+    return !empty($_SESSION["Username"]);
 }
 
 //funzione che mi registra l'utente loggato nella sessione 
 function registerLoggedUser($user){
     $_SESSION["Username"] = $user["Username"];
-    $_SESSION["nome"] = $user["nome"];
-    $_SESSION["cognome"] = $user["cognome"];
+    $_SESSION["Nome"] = $user["Nome"];
+    $_SESSION["Cognome"] = $user["Cognome"];
 }
-
 
 
 ?>
