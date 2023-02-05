3af636d9676d84a590a8c7f0f81986d6bada94a6
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-05 16:36:20 +0100

    Updated login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,24 @@
-function myFunction() {
-    alert("Password o Username sbagliati!");
-};
 
-axios.get('php/api-login.php').then(response => {
-    
+axios.get('../php/api-login.php').then(response => {
+    visiblePassw();
     let btn_submit = document.getElementById("btn_submit");
     btn_submit.addEventListener("click", () => {
-        console.log(response);
+        console.log(response.data);
         if(response.data["logineseguito"]){
-            window.location.replace("./home.php");   
+            window.location.replace("../php/home.php");   
         }else{
             alert("Password o Username sbagliati!");
-            //myFunction();
             window.location.reload;  
         }
     });
-});
+}).catch((err) => console.log(err));
+
+function visiblePassw(){
+    document.getElementById('showPass').onclick = function() {
+        if ( !this.checked ) {
+           document.getElementById('Password').type = "password";
+        } else {
+           document.getElementById('Password').type = "text";
+        }
+    }; 
+}
