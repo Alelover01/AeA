5fc5f0deb5fbbfd85eec7dfe1762d55a08e2b029
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-05 16:35:14 +0100

    Updated login.php

--- php/login.php
+++ php/login.php
@@ -19,9 +19,9 @@
         <header class="py-4">
             <h1 class="font-monospace fw-bold text-center">See&GO - Login</h1>
         </header>
-        <form action="login.php" method="post">
-            <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?></p>
+        <form method="POST">
+            <?php if (isset($_GET["error"])) { ?>
+            <p class="error"><?php echo $_GET["error"]; ?></p>
             <?php } ?>
             <h5 class="font-monospace text-center fw-bold mt-3 mb-0">Login</h5>
             <div class="col-md-8 offset-md-2 col-lg-8 offset-lg-2 div-wrapper d-flex justify-content-center align-items-center">
@@ -32,35 +32,44 @@
                             <!-- Username input -->
                             <div class="form-outline mb-4 row">
                                 <p class="col">Username:</p>
-                                <input type="username" id="username" class="col form-control" placeholder="Username"/>
-                                <label class="form-label" for="username"></label>
+                                <input type="username" id="Username" 
+                                value="<?php if(isset($_COOKIE["user_login"])) { echo $_COOKIE["user_login"]; } ?>" 
+                                class="col form-control" placeholder="Username"/>
+                                <label class="form-label" for="Username"></label>
                             </div>
 
                             <!-- Password input -->
                             <div class="form-outline mb-4 row">
                                 <p class="col">Password:</p>
-                                <input type="password" id="password" class="col form-control" placeholder="Password"/>
-                                <label class="form-label" for="password"></label>
+                                <input type="password" id="Password" 
+                                value="<?php if(isset($_COOKIE["userpassword"])) { echo $_COOKIE["userpassword"]; } ?>"
+                                class="col form-control" placeholder="Password"/>
+                                <label class="form-label" for="Password"></label>
                             </div>
 
                             <!-- 2 column grid layout for inline styling -->
                             <div class="row mb-4">
-                                <div class="col d-flex justify-content-center align-items-center">
+                            <div class="col d-flex justify-content-center align-items-center">
                                 <!-- Checkbox -->
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" value="" id="remeberMe" checked />
-                                        <label class="form-check-label" for="remeberMe"> Remember me </label>
+                                <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="showPass"  />
+                                        <label class="form-check-label" for="showPass"> Show Password </label>
                                     </div>
                                 </div>
-                                <div class="col">
+                                <div class="col form-check">
+                                    <!-- Checkbox -->
+                                        <div><input type="checkbox" name="remember" id="remember" 
+                                        <?php if(isset($_COOKIE["user_login"])) { ?> checked <?php } ?> />
+                                        <label for="remember-me">Remember me</label>
+                                    
                                     <!-- Simple link -->
-                                    <a href="#!">Forgot password?</a>
+                                    <!--<a href="#!">Forgot password?</a>-->
                                 </div>
                             </div>
 
                             <!-- Submit button -->
                             <div class=" text-center row">
-                                <button id="btn_submit" type="submit" class="btn btn-primary btn-block mb-4">Login</button>
+                                <button id="btn_submit" type="submit" class="btn btn-primary btn-block mt-2 mb-4">Login</button>
                             </div>
                             
 
@@ -74,6 +83,8 @@
             </div>    
         </form>
     </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script src="../js/login.js"></script>
 </body>
