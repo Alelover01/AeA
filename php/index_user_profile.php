24d63d5a135c810cf6fe5bb5e1dbb45ab073c38d
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-08 18:24:59 +0100

    Added index_user_profile.php

---
+++ php/index_user_profile.php
@@ -0,0 +1,19 @@
+<?php 
+require_once "../db/bootstrap.php";
+
+    $templateParams["profile"] = array("Username" => $_SESSION["Username"]);
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("Nome" => $dbh->getUserParams($_SESSION["Username"])[0]["Nome"]));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("Cognome" => $dbh->getUserParams($_SESSION["Username"])[0]["Cognome"]));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("Città" => $dbh->getUserParams($_SESSION["Username"])[0]["Città"]));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("Paese" => $dbh->getUserParams($_SESSION["Username"])[0]["Paese"]));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("numFollower" => count($dbh->getFollowing($_SESSION["Username"]))));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("numFollowed" => count($dbh->getFollowed($_SESSION["Username"]))));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("numFoto" => count($dbh->getFotoPost($_SESSION["Username"]))));
+    $templateParams["profile"] = array_merge($templateParams["profile"], array("Foto" => $dbh->getUserParams($_SESSION["Username"])[0]["Foto"]));
+     
+    $user_profile = true;
+
+
+require_once "../php/profile.php"; //?
+
+?>
