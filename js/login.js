52071098e9b286117309c1515cec1e0b8deff580
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-08 18:14:53 +0100

    updated login.js

--- js/login.js
+++ js/login.js
@@ -1,24 +1,31 @@
+const btn_check = document.getElementById('showPass').onclick = function() {
+    if ( !this.checked ) {
+        document.getElementById('Password').type = "password";
+    } else {
+        document.getElementById('Password').type = "text";
+    }
+}; 
 
-axios.get('../php/api-login.php').then(response => {
-    visiblePassw();
-    let btn_submit = document.getElementById("btn_submit");
-    btn_submit.addEventListener("click", () => {
+const btn_submit = document.getElementById("btn_submit");
+   btn_submit.addEventListener("click", function (event) {
+	event.preventDefault();
+	const username = document.querySelector("#Username").value;
+	const password = document.querySelector("#Password").value;
+	login(username, password);
+});
+
+function login(username, password) {
+    const formData = new FormData();
+    formData.append('Username', username);
+    formData.append('Password', password);
+    axios.post('../php/api-login.php', formData).then(response => {
         console.log(response.data);
-        if(response.data["logineseguito"]){
-            window.location.replace("../php/home.php");   
-        }else{
-            alert("Password o Username sbagliati!");
-            window.location.reload;  
+		if(response.data["logineseguito"]) {
+			window.location.pathname = './AeA-main/php/home.php';
+		}else{
+            alert("Password e/o Username sbagliati!");
+            window.location.reload();
+             
         }
     });
-}).catch((err) => console.log(err));
-
-function visiblePassw(){
-    document.getElementById('showPass').onclick = function() {
-        if ( !this.checked ) {
-           document.getElementById('Password').type = "password";
-        } else {
-           document.getElementById('Password').type = "text";
-        }
-    }; 
 }
