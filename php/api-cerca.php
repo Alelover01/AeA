89bf74cc5c6fc4d291694cfabb88f3404e597a41
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-08 18:21:51 +0100

    Added api-cerca.php

---
+++ php/api-cerca.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../db/bootstrap.php';
+require_once '../db/database.php';
+
+$result["searchSuccess"] = false;
+
+	if(isset($_POST["usrSearch"])) {
+
+		if(($dbh->userInDb($_POST["usrSearch"]))!=0) {
+            $usrSearch = $dbh->researchUser($_POST["usrSearch"]);
+			$result["searchSuccess"] = true;
+			$_SESSION["usrSearch"] = $usrSearch[0]["Username"];//$_POST["usrSearch"];//$login_result[0]["Username"];
+            }
+		}else{
+            //ricerca fallita
+            $result["error"] = "Utente inesistente \n o Username sbagliato!";
+        }
+    
+	header('Content-Type: application/json');
+	echo json_encode($result);
+?>
