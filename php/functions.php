1adbaf7fc85d4c40df0ffcd6a99d74cc99c60813
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-08 18:27:01 +0100

    Updated functions.php

--- php/functions.php
+++ php/functions.php
@@ -1,8 +1,10 @@
 <?php
-
-//funzione che mi dice se un utente è loggato
+//funzione che mi dice se un utente è loggato 
 function isUserLoggedIn(){
-    return !empty($_SESSION["Username"]);
+    if ($_SESSION["Username"] != ""){
+        return true;
+    };
+    return false;
 }
 
 //funzione che mi registra l'utente loggato nella sessione 
@@ -10,6 +12,13 @@ function registerLoggedUser($user){
     $_SESSION["Username"] = $user["Username"];
     $_SESSION["Nome"] = $user["Nome"];
     $_SESSION["Cognome"] = $user["Cognome"];
+}
+
+//funzione che mi registra l'utente cercato nella sessione
+function registerSearchedUser($user){
+    $_SESSION["usrSearch"] = $user["Username"];
+   /* $_SESSION["Nome"] = $user["Nome"];
+    $_SESSION["Cognome"] = $user["Cognome"];*/
 }
 
 
