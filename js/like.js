e3ae257d8dee27e747aafcf636558235bcb0ea94
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-11 11:19:54 +0100

    Added like.js

---
+++ js/like.js
@@ -0,0 +1,15 @@
+const btn_like = document.getElementById("btn_like");
+btn_like.addEventListener("click", function(event){
+        event.preventDefault();
+        axios.get('../php/api-like.php').then(function (response) {
+                console.log(response.data);
+                if(!response.data) {
+                    alert("ERRORE RILEVATO!");
+                    window.location.reload();   
+                }else{
+                    window.location.reload();   
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+});
