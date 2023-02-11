770ffdd19c4e19e89bdb1a6cb7aedf8f7dd28536
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-11 11:32:02 +0100

    Upload index file php

---
+++ php/index_post_other.php
@@ -0,0 +1,13 @@
+<?php 
+require_once "../db/bootstrap.php";
+
+$templateParams["profile"] = array("Username" => $_SESSION["usrSearch"]);
+$_SESSION['post_id']=$_GET['post_id'];
+$templateParams["post"] = array('post_id' => $_GET['post_id']);
+$templateParams["profile"] = array_merge($templateParams["profile"], array("Foto" => $dbh->getUserParams($_SESSION["usrSearch"])[0]["Foto"]));
+$templateParams["post"] = array_merge($templateParams["post"], array("created_time" => $dbh->getPostUser($_SESSION["usrSearch"], $_SESSION["post_id"])[0]["created_time"]));
+$templateParams["post"] = array_merge($templateParams["post"], array("caption" => $dbh->getPostUser($_SESSION["usrSearch"], $_SESSION["post_id"])[0]["caption"]));
+$templateParams["post"] = array_merge($templateParams["post"], array("media_file" => $dbh->getPostUser($_SESSION["usrSearch"], $_SESSION["post_id"])[0]["media_file"]));
+$templateParams["post"] = array_merge($templateParams["post"], array("numLikes" => count($dbh->getLikesPost( $_SESSION["post_id"]))));
+
+require_once "../php/post.php"; 
