9c816490c37df58815330ced82c5bf6b248fcbba
Eleonora Falconi <83820248+Eleonora-f@users.noreply.github.com>
2023-02-12 14:53:33 +0100

    added pubblica.js

---
+++ js/pubblica.js
@@ -0,0 +1,24 @@
+const btn_pubblica = document.getElementById("btn_pubblica");
+    btn_pubblica.addEventListener("click", function(event){
+        event.preventDefault();
+        const btnFile = document.getElementById("btnFile").files[0];
+        const txtDidascalia = document.querySelector("#txtDidascalia").value;
+        const cbCategoria = document.querySelector("#cbCategoria").value;
+        pubblica(btnFile, txtDidascalia, cbCategoria );
+    });
+
+function pubblica(btnFile, txtDidascalia, cbCategoria) {
+    const formData = new FormData();
+    formData.append('btnFile', btnFile);
+    formData.append('txtDidascalia', txtDidascalia);
+    formData.append('cbCategoria', cbCategoria);
+    axios.post('../php/api-post.php', formData).then(response => {
+        console.log(response.data);
+		if(response.data) {
+			window.location.pathname = './AeA-main/php/home.php';
+		}else{
+            alert("Errore, riprova!");
+            window.location.reload();
+        }
+    });
+}
