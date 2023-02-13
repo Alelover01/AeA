c38b33cee921390a17db9933e61677e76abdaf29
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 10:03:34 +0100

    Added delete.js

---
+++ js/delete.js
@@ -0,0 +1,15 @@
+const btn_delete = document.getElementById("btn_delete");
+btn_delete.addEventListener("click", function(event){
+        event.preventDefault();
+        axios.get('../php/api-delete.php').then(function (response) {
+                console.log(response.data);
+                if(!response.data) {
+                    alert("ERRORE! RIPROVA!");
+                    window.location.reload();   
+                }else{
+                window.location.pathname = './AeA-main/php/index_user_profile.php';  
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
+});
