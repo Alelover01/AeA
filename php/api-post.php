4dbaa93de1e6cdf353c1d62e0c2be1bcfe6888e0
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 09:58:43 +0100

    Updated api-post.php

--- php/api-post.php
+++ php/api-post.php
@@ -21,10 +21,13 @@
 		  if (empty($_POST['cbCategoria'])) {
 			echo 'La categoria è obbligatoria';
 		  }
+          $uploadDirectory = "../images/";
+          $uploadFileName = $uploadDirectory . $_FILES['btnFile']['name'];
+          move_uploaded_file($_FILES['btnFile']['tmp_name'], $uploadFileName);
 		  $created_time = date("Y-m-d H:i:s");
 		  $idCategoria=$dbh->getIdPostByPostName($_POST["cbCategoria"]);
 		  $idPost=$dbh->getLastIdPost();
-		  $dbh->createPost($idPost[0]["post_id"]+1, $_SESSION["Username"], $_FILES['btnFile']['name'], $created_time, $_POST["txtDidascalia"], $idCategoria[0]["post_type_id"]);
+		  $dbh->createPost($idPost[0]["post_id"]+1, $_SESSION["Username"], $uploadFileName, $created_time, $_POST["txtDidascalia"], $idCategoria[0]["post_type_id"]);
 		  $result["success"]=true;
 	 }
 
