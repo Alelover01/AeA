14195be4f1e5ac96e5f4d74badc0637a3f6cf453
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 18:25:02 +0100

    Updated like.js

--- js/like.js
+++ js/like.js
@@ -1,25 +1,4 @@
-/*const btn_like = document.getElementById("btn_like");
-btn_like.addEventListener("click", function(event){
-        event.preventDefault();
-        const postId = this.dataset.postId;
-        like(postId);
-});
 
-function like(postId){
-    const formData = new FormData();
-    formData.append('postId', postId);
-    axios.post('../php/api-like.php',formData).then(response => {
-        console.log(response.data);
-        if(!response.data) {
-            alert("ERRORE RILEVATO!");
-            window.location.reload();   
-        }else{
-            window.location.reload();   
-        }
-    }).catch(function (error) {
-        console.log(error);
-    });
-}*/
 const btn_like = document.getElementById("btn_like");
 btn_like.addEventListener("click", function(event){
         event.preventDefault();
