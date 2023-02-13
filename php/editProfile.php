62a470a8c32ee26d466a4ee90ba44531babe85e6
Alesja Delja <alesja.delja@studio.unibo.it>
2023-02-13 15:55:41 +0100

    cambiato nome script

--- php/editProfile.php
+++ php/editProfile.php
@@ -81,6 +81,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-        <script src="../js/pubblica.js"></script>
+        <script src="../js/change.js"></script>
 </body>
 </html>
