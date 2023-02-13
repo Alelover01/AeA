54bf8e27c147f9f3c4491aaf9441b89b088d687d
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 18:47:14 +0100

    Updated api-login.php

--- php/api-login.php
+++ php/api-login.php
@@ -12,20 +12,7 @@ $result["logineseguito"] = false;
 			$result["logineseguito"] = true;
 			$_SESSION["Username"] = $login_result[0]["Username"];
             $_SESSION["Password"] = $login_result[0]["Password"];
-
-            if(!empty($_POST["remember"])) {//NON ho capito se funziona o no
-                //COOKIES for username
-                setcookie ("user_login",$_POST["Username"],time()+ (10 * 365 * 24 * 60 * 60));
-                //COOKIES for password
-                setcookie ("userpassword",$_POST["Password"],time()+ (10 * 365 * 24 * 60 * 60));
-                } else {
-                    if(isset($_COOKIE["user_login"])) {
-                        setcookie ("user_login","");
-                        if(isset($_COOKIE["userpassword"])) {
-                            setcookie ("userpassword","");
-                        }
-                    }
-                }
+            
 		}else{
             //Login fallito
             $result["error"] = "Username e/o password errati";
