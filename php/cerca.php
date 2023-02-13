15ecfdf335edd6b7318be3d0bda42205491a72d4
Eleonora Falconi <83820248+Eleonora-f@users.noreply.github.com>
2023-02-13 15:35:14 +0100

    modified

--- php/cerca.php
+++ php/cerca.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="it">
-    <head>
-        <link rel="shortcut icon" href="../images/logo.jpg">
-        <title>See&Go - Homepage</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/cerca_style.css" >
         <link rel="stylesheet" href="../css/menu_style.css" >
         <script src="../js/menu.js"></script>
-        
-        
-  </head>
-  <body class="bg-light">
-    <header class="py-3 text-white bg-dark">
-      <h1 class="font-monospace text-center">See&Go - Homepage</h1>
-    </header> 
-    <!-- MENU A COMPARSA -->
-              <aside>
+    
+
+    <title>See&GO - Login</title>
+    <link rel="shortcut icon" href="../images/logo.jpg">
+</head>
+<body>
+<main>
+        <!-- Section: Background image -->
+<section  class=" bg-image" style=" background-image: url(../images/Rockies_in_the_morning.jpg) ; width: 100%; height: 100%;; background-size: cover; background-repeat: no-repeat;">
+        <header class="py-4">
+            <h1 class="font-monospace fw-bold text-center">See&GO - EXPLORE</h1>
+        </header>
+        <aside>
                 <button class="hamburger">
                     <div class="bar"></div>
                 </button>
@@ -40,78 +42,61 @@
                         </li>
                     </ul>
                 </nav>
-        
-            </aside>
-
-    <main>
-    <input type="usrSearch" id="usrSearch" name="usrSearch" class="col form-control" placeholder="Search"/>
-    <label class="form-label" for="search"></label>
-    <button id="btn_search" name="btn_search" type="submit" class="btn btn-primary btn-block mt-2 mb-4">Cerca</button>
-        <div class="container-fluid p-0 overflow-hidden">
-            <div class="py-4 px-4"> 
-                <div class="row align-items-start ">
-                    <?php  require_once '../db/bootstrap.php';
-                            $categories=$dbh->getPost_Type();
-                            //var_dump($categories);
-                            foreach($categories as $type_cat):?>
-                            <?php //var_dump($type_cat)?>
-                    <div class="col-md-4">
-                        <div class="box box-widget widget-user-2">
-                            <div class="bg-image card shadow-1-strong" style="background-image: url('https://www.labsus.org/wp-content/uploads/2018/05/architecture-3095716_1920-1140x760.jpg'); width: 100%; height: 100%;">
-                                <div class="d-grid gap-2 col-6 mx-auto">
-                                    <a href='postCategoria.php?id=<?php echo $type_cat["post_type_id"];?>'>
-                                        <button class="btn btn-outline-dark btn-lg btn-block mt-2 mb-2 fw-bold text-center" id="<?php echo $type_cat["post_type_id"];?>" name="btnCategory" type="button">
-                                        <?php echo $type_cat["post_type_name"];?></button>
-                                    </a> 
-                                </div>
-                            </div>
+            </aside>  
+    <h5 class="font-monospace text-center fw-bold mt-3 mb-0">Search what you like!</h5>
+        <div class="col-md-4 offset-md-2 col-lg-8 mt-3 mb-3 offset-lg-2 div-wrapper d-flex justify-content-center align-items-center">
+            <div class="row ">
+                        <div class="col-lg-7">
+                            <label class="form-label text-center" for="search"></label>
+                            <input type="usrSearch" id="usrSearch" name="usrSearch" class="d-grid gap-2 col-6 mx-auto form-control" placeholder="Search"/>
                         </div>
-                    </div>
-                    <?php endforeach; ?>
-                </div>
+                        <div class="col-sm">
+                            <button id="btn_search" name="btn_search" type="submit" class="btn btn-primary btn-block mt-4 mb-4">Go</button>
+                        </div>
             </div>
         </div>
-        </div>
-        <div class="py-4 px-4"> 
-            <div class="d-flex align-items-center justify-content-between mb-3"> 
-                <h5 class="mb-0">Photos recommended</h5>
-                <a href="#" class="btn btn-link text-muted">Show all</a> 
-            </div> 
-            <div class="row"> 
-                <div class="col-lg-4 mb-4 pr-lg-1">
-                    <img src="https://images.unsplash.com/photo-1469594292607-7bd90f8d3ba4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" style="width: 80%; height: 100%;" alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-                <div class="col-lg-4 mb-4 pl-lg-1">
-                    <img src="https://images.unsplash.com/photo-1493571716545-b559a19edd14?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80" style="width: 80%; height: 100%;" alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-                <div class="col-lg-4 mb-4 pr-lg-1">
-                    <img src="https://images.unsplash.com/photo-1453791052107-5c843da62d97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" style="width: 80%; height: 100%;"  alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-                <div class="col-lg-4 mb-4 pl-lg-1">
-                    <img src="https://www.travel365.it/foto/come-muoversi-a-parigi-come-arrivare-e-mezzi-consigliati.png" style="width: 80%; height: 80%;"  alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-                <div class="col-lg-4 mb-4 pl-lg-1">
-                    <img src="https://c8.alamy.com/compit/kjwwda/una-illustrazione-vettoriale-di-cucina-spagnola-cucina-kjwwda.jpg" style="width: 80%; height: 100%;"  alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-                <div class="col-lg-4 mb-4 pl-lg-1">
-                    <img src="https://www.grazia.it/content/uploads/2019/12/04_mix_match_montagna.jpg" style="width: 80%; height: 100%;"  alt="" class="img-fluid rounded shadow-sm">
-                </div> 
-            </div>
-
+    <div class="col-lg-8  offset-lg-2  d-flex justify-content-center align-items-center">
+        <!-- <div class="container-fluid p-0 overflow-hidden"> -->
+            <div class="card shadow " style="border-radius: 1rem;">
             
-            <!-- FOOTER -->
-        <div class="row">
+                        <div class="py-4 px-4"> 
+                            <div class="row align-items-start ">
+                                <?php  require_once '../db/bootstrap.php';
+                                        $categories=$dbh->getPost_Type();
+                                        foreach($categories as $type_cat):?>
+                                        <?php ?>
+                                    <div class="col-md-4">
+                                        <div class="box box-widget widget-user-2">
+                                            <div class="bg-image card shadow-1-strong text-center" style="background-color: #20c997; width: 100%; height: 100%;">
+                                                        <div class="d-grid gap-2 col-5 mx-auto">
+                                                            <a href='postCategoria.php?id=<?php echo $type_cat["post_type_id"];?>'>
+                                                                <button class="btn btn-outline-dark btn-lg btn-block mt-2 mb-2 fw-bold text-center" id="<?php echo $type_cat["post_type_id"];?>" name="btnCategory" type="button">
+                                                                <?php echo $type_cat["post_type_name"];?></button>
+                                                            </a> 
+                                                        </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                <?php endforeach; ?>
+                            </div>
+             </div>
+        <!-- </div> -->
+    </div>
+</section> 
+         <!-- FOOTER -->
+         <div class="row">
             <div class="col-12">
-                    <footer class="py-3 text-white bg-dark">
+                    <footer class="py-3 text-white bg-dark ">
                         <p class="font-monospace text-center">See&Go</p>
                     </footer>   
             </div>   
-        </div>       
+        </div>  
+        
+    
     </main>
-    <script src="../js/cerca.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-  </body>
+    <script src="../js/login.js"></script>
+</body>
 </html>
-
