2d932f2b5ac00397ff7eb286938a7db05feb2fd8
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 18:20:26 +0100

    updated change.js

--- js/change.js
+++ js/change.js
@@ -1,8 +1,19 @@
-function change() {
-    let newFoto = document.getElementById('btnFile');
-    let newCity = document.getElementById('lbCity');
-    let newPass = document.getElementById('lbPass');
-
-    let url = '../php/api-editProfile.php?foto=' + newFoto + '&city=' + newCity + 'pass=' + newPass;
+function changeFoto() {
+    let newFoto = document.getElementById('btnFile').files[0];
+    let foto = newFoto.name; 
+    let action = 1;
+    let url = '../php/api-editProfile.php?foto=' + foto + '&action='+ action;
+    window.location.href = url;
+}
+function changeCity() {
+    let newCity = document.getElementById('lbCity').value;
+    let action = 2;
+    let url = '../php/api-editProfile.php?city=' + newCity + '&action='+ action;
+    window.location.href = url;
+}
+function changePassword() {
+    let newPass = document.getElementById('lbPass').value;
+    let action = 3;
+    let url = '../php/api-editProfile.php?pass=' + newPass + '&action=' + action;
     window.location.href = url;
 }
