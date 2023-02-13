87b14fa5f50ec2272fd3d3c28aeaa11638022e00
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 09:56:06 +0100

    Added delete.php

---
+++ php/api-delete.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../db/bootstrap.php';
+require_once '../db/database.php';
+
+$result["success"] = false;
+
+    $dbh->deletePost($_SESSION["post_id"]);
+    $dbh->deleteCommentsPost($_SESSION["post_id"]);
+    if(count($dbh->getPostByType($_SESSION["post_id"]))==0 && $dbh->deleteCommentsPost($_SESSION["post_id"])==true){
+        $result["success"] = true;
+    }
+
+	header('Content-Type: application/json');
+	echo json_encode($result);
+?>
