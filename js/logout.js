9d02c7b68725afd7d36b9efb85651f3baa77a1d7
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-13 10:04:18 +0100

    Updated logout.php

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,17 @@
-axios.get('php/api-logout.php').then(response => {
-    if(response.data["logouteseguito"]){
-        window.location.replace("./login.php");   
-    }else{
-        //?
-    }
-});
+
+const btn_logout = document.getElementById("btn_logout");
+btn_logout.addEventListener("click", function(event) {
+    event.preventDefault();
+	axios.get('../php/api-logout.php').then(response => {
+        console.log(response.data);
+        if(response.data){
+            window.location.pathname = './AeA-main/php/login.php'; 
+        }else{
+            alert("Loguot NON eseguito");
+            window.location.reload;
+        }
+    });
+}
+
+);
+
