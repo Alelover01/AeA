ac8f4dd5d8001454539146bcc1f2018c2ad219cc
Anna Bartolucci <93584752+nuxbart@users.noreply.github.com>
2023-02-15 00:17:25 +0100

    Update pubblica.js

--- js/pubblica.js
+++ js/pubblica.js
@@ -14,7 +14,7 @@ function pubblica(btnFile, txtDidascalia, cbCategoria) {
     formData.append('cbCategoria', cbCategoria);
     axios.post('../php/api-post.php', formData).then(response => {
         console.log(response.data);
-		if(response.data==true) {
+		if(response.data) {
 			window.location.pathname = './AeA-main/php/index_user_profile.php';
 		}else{
             alert("Errore, Riprova!");
